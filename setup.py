5c2696d368839d73bb8193ce8d6e4de994258dba
GopiSimple <139834984+GopiSimple@users.noreply.github.com>
2023-07-21 13:37:27 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='your_module_name',
+    version='0.1',
+    packages=['your_module_name'],
+    install_requires=[
+        # List any dependencies here
+    ],
+    author='Your Name',
+    author_email='your@email.com',
+    description='A brief description of your module',
+    url='https://github.com/your_username/your_module',
+)
